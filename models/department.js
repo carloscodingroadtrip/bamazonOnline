da6c7469ce0a8fbe97f3655c4212b57da2275ba3
Mohammed Hameed <mohammed.hameed@email.com>
2018-05-05 18:23:24 -0500

    some changes

--- models/department.js
+++ models/department.js
@@ -1,8 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
-    var Todo = sequelize.define("deparmtent", {
+    var deparmtent = sequelize.define("deparmtent", {
       name: DataTypes.STRING,
       over_head_costs: DataTypes.FLOAT
     });
-    return Todo;
+    return deparmtent;
   };
   
