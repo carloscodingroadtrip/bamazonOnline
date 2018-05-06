fc1eb9f608920a4618a743434c2aaad63f16c878
Carlos Molina <molina.carlos.dev@gmail.com>
2018-05-06 09:37:10 -0500

    Added all routes and pages for the admin user

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -5,5 +5,30 @@ router.get('/', (req,res)=>{
     res.render('./admin/index');
 });
 
+//********* Admin ROUTES *************/
+//Adding new department
+router.get('/departments', (req,res)=>{
+    res.render('./admin/add-dept');
+});
+
+//Adding new products
+router.get('/addproducts', (req,res)=>{
+    res.render('./admin/addprod');
+});
+
+//Adding view low inventory
+router.get('/viewlowinventory', (req,res)=>{
+    res.render('./admin/lowinventory');
+});
+
+//Adding to inventory
+router.get('/addtoinventory', (req,res)=>{
+    res.render('./admin/add-inventory');
+});
+
+//Sales report
+router.get('/salesreport', (req,res)=>{
+    res.render('./admin/salesreport');
+});
 
 module.exports = router;
